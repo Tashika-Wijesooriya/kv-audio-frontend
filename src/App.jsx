c1e6eb1546f060a13a0b892c74c71151d0603b8d
Tashika Wijesooriya <it22644244@my.sliit.lk>
2025-03-12 14:12:02 +0530

    create Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AdminPage from "./assets/components/adminPage";
+import AdminPage from "./pages/admin/adminPage";
+import HomePage from "./pages/home/homePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes path="/*">
         <Route path="/admin/*" element={<AdminPage />} />
+        <Route path="/*" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/home/homePage.jsx
@@ -0,0 +1,10 @@
+import Header from "../../assets/components/header";
+
+export default function HomePage()
+{
+    return (
+        <div className="w-full h-screen flex   justify-center">
+           <Header/>
+       </div>
+    )
+}
